59a2c181a55ca2fa37d3b8f214df6c6875b2166c
mowatermelon <863421510@qq.com>
2018-01-23 09:15:10 +0800

    添加elif案例

--- class_1/testIf.py
+++ class_1/testIf.py
@@ -3,10 +3,20 @@
 x = int(input("Please enter an integer: "))
 
 if x<1:
-	print('ererwerw')
+	print('x is lower than 1')
 else:
-	print('ewrwerwdfsdfs')
+	print('x is bigger than 1')
 
+# learn if and elif in python
+if x < 0:
+	x = 0
+	print('Negative changed to zero')
+elif x == 0:
+	print('Zero')
+elif x == 1:
+	print('Single')
+else:
+	print('More')
 
 
 
